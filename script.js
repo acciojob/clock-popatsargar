d1c24c58a6a0dcea642d8cc652eb4ce06dfa07d3
Popat Bhausaheb Sargar <sargarpopat219@gmail.com>
2026-01-17 15:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timer = document.getElementById("timer");
+    const now = new Date();
+    timer.textContent = now.toString();
+}
+ 
+updateTimer();
+ 
+setInterval(updateTimer, 1000);
